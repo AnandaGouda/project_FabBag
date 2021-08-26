72377badb2222592b5c5cee0e742a6b04c142e28
AnandaGouda <ananda_fw10_072@masai.school>
2021-08-26 16:10:57 +0530

    cart part complete and some chance in index part

--- addtocart.js
+++ addtocart.js
@@ -14,3 +14,37 @@ function hideupnav(){
 }
 
 //===============================================
+function planchange(no){
+    var price = document.getElementById("price");
+    if(no==1){
+        price.innerText="599";
+    }else if(no==3){
+        price.innerText="1599";
+    }else if(no==6){
+        price.innerText="2899";
+    }else if(no==12){
+        price.innerText="4999";
+    }
+}
+//=======================================
+function increaseQunt(){
+    let qunt = document.getElementById("Qunt");
+    qunt.innerText=Number(qunt.innerText)+1;
+    
+}
+function decreaseQunt(){
+    let qunt = document.getElementById("Qunt");
+    if(Number(qunt.innerText)==1){
+        qunt.innerText=1;
+    }else{
+        qunt.innerText=Number(qunt.innerText)-1;
+    }
+    
+}
+function gotocart(){
+    var price=document.getElementById("price").innerText;
+    var qnt= document.getElementById("Qunt").innerText;
+    var total=Number(price)*Number(qnt);
+    localStorage.setItem("total", JSON.stringify(total));
+    window.location="cart.html"
+}
---
+++ cart.js
@@ -0,0 +1,32 @@
+function showsidenav(){
+    document.getElementById("sidenav").style.display="block";
+}
+function hidesidenav(){
+    document.getElementById("sidenav").style.display="none";
+}
+function showupnav(){
+    document.getElementById("upnav").style.display="block";
+}
+function hideupnav(){
+    document.getElementById("upnav").style.display="none";
+}
+
+//===============================================
+   var total=document.getElementById("rs");
+   var tp = JSON.parse(localStorage.getItem("total"));
+   total.innerText=`Rs.${tp}.00`;
+   document.getElementById("qty").value=1;
+   document.getElementById("count").innerText=1;
+   //=========================
+
+   function cancel(){};
+
+
+   function update(){
+       let cart =document.getElementById("count");
+       let qty =document.getElementById("qty").value;
+       cart.innerText=qty;
+       let price=tp*qty;
+       total.innerText=`Rs.${price}.00`;
+
+   }
